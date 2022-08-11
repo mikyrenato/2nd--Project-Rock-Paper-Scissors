9981d1d391f0eeef9dd7e1953cda8334f131a297
mikyrenato <mikyrenato@yahoo.com>
2022-08-11 13:17:04 +0000

    Fixing js bugs using jshint.com as mentor suggested

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-const choiceButtons = document.querySelectorAll('[data-selection]')
-const lastColumn = document.querySelector('[data-final-column]')
-const computerResult = document.querySelector('[data-computer-score]')
-const yourResult = document.querySelector('[data-your-score]')
+const choiceButtons = document.querySelectorAll('[data-selection]');
+const lastColumn = document.querySelector('[data-final-column]');
+const computerResult = document.querySelector('[data-computer-score]');
+const yourResult = document.querySelector('[data-your-score]');
 const input = document.getElementById('nickname');
 const start = document.getElementById('start');
 const you = document.getElementById('nick');
@@ -23,27 +23,27 @@ const SELECTIONS = [
     emoji: '✌',
     beats: 'paper'
   }
-]
+];
 
 
 choiceButtons.forEach(selectionButton => {
   selectionButton.addEventListener('click', e => {
-    const selectionName = selectionButton.dataset.selection
-    const selection = SELECTIONS.find(selection => selection.name === selectionName)
-    makeChoice(selection)
-  })
-})
+    const selectionName = selectionButton.dataset.selection;
+    const selection = SELECTIONS.find(selection => selection.name === selectionName);
+    makeChoice(selection);
+  });
+});
 
 function makeChoice(selection) {
-  const computerSelection = randomChoice()
-  const yourWinner = Winner(selection, computerSelection)
-  const computerWinner = Winner(computerSelection, selection)
+  const computerSelection = randomChoice();
+  const yourWinner = Winner(selection, computerSelection);
+  const computerWinner = Winner(computerSelection, selection);
 
-  addChoiceResult(computerSelection, computerWinner)
-  addChoiceResult(selection, yourWinner)
+  addChoiceResult(computerSelection, computerWinner);
+  addChoiceResult(selection, yourWinner);
 
-  if (yourWinner) incrementScore(yourResult)
-  if (computerWinner) incrementScore(computerResult)
+  if (yourWinner) incrementScore(yourResult);
+  if (computerWinner) incrementScore(computerResult);
 
   y.innerHTML = selection.emoji;
   o.innerHTML = computerSelection.emoji;
@@ -51,29 +51,29 @@ function makeChoice(selection) {
 }
 
 function incrementScore(scoreSpan) {
-  scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+  scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1;
   checkWinner();
 }
 
 function addChoiceResult(selection, winner) {
-  const div = document.createElement('div')
-  div.innerText = selection.emoji
-  div.classList.add('resultchoice')
-  if (winner) div.classList.add('winner')
-  lastColumn.after(div)
+  const div = document.createElement('div');
+  div.innerText = selection.emoji;
+  div.classList.add('resultchoice');
+  if (winner) div.classList.add('winner');
+  lastColumn.after(div);
 }
 
 function Winner(selection, opponentSelection) {
-  return selection.beats === opponentSelection.name
+  return selection.beats === opponentSelection.name;
 }
 
 function randomChoice() {
-  const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
-  return SELECTIONS[randomIndex]
+  const randomIndex = Math.floor(Math.random() * SELECTIONS.length);
+  return SELECTIONS[randomIndex];
 }
 
 function nickname() {
-  nick.innerHTML = input.value;
+  you.innerHTML = input.value;
   input.value = "";
   document.getElementById("start").style.visibility = "hidden";
   document.getElementById("nickname").style.visibility = "hidden";
@@ -82,8 +82,8 @@ function nickname() {
   start.addEventListener('click', nickname);
 
   function checkWinner() {
-    let computerResult = document.querySelector('[data-computer-score]').innerHTML
-    let yourResult = document.querySelector('[data-your-score]').innerHTML
+    let computerResult = document.querySelector('[data-computer-score]').innerHTML;
+    let yourResult = document.querySelector('[data-your-score]').innerHTML;
     if (computerResult >= 10) {
       alert(":( You lost, better luck next time.");
       location.reload();
@@ -93,5 +93,5 @@ function nickname() {
       location.reload();
     }
     else
-    return false
+    return false;
 }
